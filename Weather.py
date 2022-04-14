f0f426d0255b256f05d90e682d152600a9c53111
Flin <79360508+Saygone@users.noreply.github.com>
2022-04-14 23:30:42 +0300

    Add files via upload
    Текущая погода голосом

--- Weather.py
+++ Weather.py
@@ -1,39 +1,38 @@
-import requests, json
+import speech_recognition as sr
+import pyttsx3
+from bs4 import BeautifulSoup
+import requests
 
-api_key = "693c7ce12add5269e8785cf51ba6e404"
+headers = {
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+# функции
+def speak(what):
+    print(what)
+    speak_engine.say(what)
+    speak_engine.runAndWait()
+    speak_engine.stop()
 
-city_name = input("Enter city name : ")
+# запуск
+r = sr.Recognizer()
+m = sr.Microphone(device_index=1)
 
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+with m as source:
+    r.adjust_for_ambient_noise(source)
 
-response = requests.get(complete_url)
+speak_engine = pyttsx3.init()
 
-x = response.json()
+def weather(city):
+    city = city.replace(" ", "+")
+    res = requests.get(
+        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',
+        headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    location = soup.select('#wob_loc')[0].getText().strip()
+    info = soup.select('#wob_dc')[0].getText().strip()
+    weather = soup.select('#wob_tm')[0].getText().strip()
+    speak(location + " " + info + " " + weather + " Градуса по цельсию")
 
-if x["cod"] != "404":
-
-    y = x["main"]
-
-    current_temperature = y["temp"]
-
-    current_pressure = y["pressure"]
-
-    current_humidity = y["humidity"]
-
-    z = x["weather"]
-
-    weather_description = z[0]["description"]
-
-    print(" Temperature (in kelvin unit) = " +
-          str(current_temperature) +
-          "\n atmospheric pressure (in hPa unit) = " +
-          str(current_pressure) +
-          "\n humidity (in percentage) = " +
-          str(current_humidity) +
-          "\n description = " +
-          str(weather_description))
-
-else:
-    print(" City Not Found ")
+city = "Киров"
+city = city+" weather"
+weather(city)
