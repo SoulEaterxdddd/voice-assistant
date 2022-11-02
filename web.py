1c26181b0bc217c2343dbd7a4f6a1ade6491d723
eye of Skadi <117259408+SoulEaterxdddd@users.noreply.github.com>
2022-11-02 10:27:35 +0300

    Update web.py

--- web.py
+++ web.py
@@ -2,26 +2,24 @@ import webbrowser as wb
 import speech_recognition as sr
 import pyttsx3
 
-
+#Определение микрофона
 r = sr.Recognizer()
 m = sr.Microphone(device_index=1)
 speak_engine = pyttsx3.init()
 
+#Синтезатор речи
 def speak(what):
     print(what)
     speak_engine.say(what)
     speak_engine.runAndWait()
     speak_engine.stop()
 
+#Поисковая система    
 def search(srch):
     wb.open(f'https://www.google.ru/search?q={srch}')
 
-#
+#преобразование запроса в текст
 with m as source:
     audio = r.listen(source)
 srch = r.recognize_google(audio, language = 'ru-RU')
 search(srch)
-
-
-
-
